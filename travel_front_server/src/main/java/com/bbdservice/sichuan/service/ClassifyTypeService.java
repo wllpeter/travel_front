a50a8872346eaafb8f670a67d592a1df77109ec2
chenyalan <1448073111@qq.com>
2018-01-05 17:52:11 +0800

    产品类别

---
+++ travel_front_server/src/main/java/com/bbdservice/sichuan/service/ClassifyTypeService.java
@@ -0,0 +1,14 @@
+package com.bbdservice.sichuan.service;
+
+import com.bbdservice.sichuan.entity.ClassifyData;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by 陈亚兰 on 2018/1/5.
+ * 旅游产品监测-旅游产品分类
+ */
+public interface ClassifyTypeService {
+    List<ClassifyData> getClassifyDataByDataTypeAndYearAndMonthAndProductType(String dataType,Long year,Long month,Long productType);
+}
