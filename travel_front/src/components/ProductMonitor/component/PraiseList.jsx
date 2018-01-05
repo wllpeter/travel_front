15eb434141dd883a16fd0a9c658ac80c535a04ab
xingyixiang <452282988@qq.com>
2018-01-05 17:22:01 +0800

    好评榜

--- travel_front/src/components/ProductMonitor/component/PraiseList.jsx
+++ travel_front/src/components/ProductMonitor/component/PraiseList.jsx
@@ -29,6 +29,153 @@ export default class PraiseList extends Component {
                 </div>
             </div>
             <div className="praise">
+                <table className="mt-table col-1-al">
+                    <thead>
+                    <tr>
+                        <th>排名</th>
+                        <th>名称</th>
+                        <th>评分</th>
+                        <th>环比</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>
+                            01
+                            <span className="praise-icon praise-icon-up">
+                                <i className="iconfont icon-up"></i>
+                                1
+                            </span>
+                        </td>
+                        <td>九寨沟</td>
+                        <td>5</td>
+                        <td>15%</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            02
+                            <span className="praise-icon  praise-icon-down">
+                                <i className="iconfont icon-down"></i>
+                                 1
+                            </span>
+                        </td>
+                        <td>西双版纳</td>
+                        <td>5</td>
+                        <td>15%</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            03
+                            <span className="praise-icon">
+                                &nbsp;&nbsp;
+                                <i className="iconfont">-</i>
+                                &nbsp;&nbsp;
+                            </span>
+                        </td>
+                        <td>都江堰</td>
+                        <td>5</td>
+                        <td>15%</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            04
+                            <span className="praise-icon praise-icon-up">
+                                <i className="iconfont icon-up"></i>
+                                1
+                            </span>
+                        </td>
+                        <td>九寨沟</td>
+                        <td>5</td>
+                        <td>15%</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            02
+                            <span className="praise-icon  praise-icon-down">
+                                <i className="iconfont icon-down"></i>
+                                 1
+                            </span>
+                        </td>
+                        <td>西双版纳</td>
+                        <td>5</td>
+                        <td>15%</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            05
+                            <span className="praise-icon">
+                                &nbsp;&nbsp;
+                                <i className="iconfont">-</i>
+                                &nbsp;&nbsp;
+                            </span>
+                        </td>
+                        <td>都江堰</td>
+                        <td>5</td>
+                        <td>15%</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            06
+                            <span className="praise-icon praise-icon-up">
+                                <i className="iconfont icon-up"></i>
+                                1
+                            </span>
+                        </td>
+                        <td>九寨沟</td>
+                        <td>5</td>
+                        <td>15%</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            07
+                            <span className="praise-icon praise-icon-up">
+                                <i className="iconfont icon-up"></i>
+                                1
+                            </span>
+                        </td>
+                        <td>九寨沟</td>
+                        <td>5</td>
+                        <td>15%</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            08
+                            <span className="praise-icon  praise-icon-down">
+                                <i className="iconfont icon-down"></i>
+                                 1
+                            </span>
+                        </td>
+                        <td>西双版纳</td>
+                        <td>5</td>
+                        <td>15%</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            09
+                            <span className="praise-icon">
+                                &nbsp;&nbsp;
+                                <i className="iconfont">-</i>
+                                &nbsp;&nbsp;
+                            </span>
+                        </td>
+                        <td>都江堰</td>
+                        <td>5</td>
+                        <td>15%</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            10
+                            <span className="praise-icon">
+                               <i className="iconfont icon-up"></i>
+                                1
+                            </span>
+                        </td>
+                        <td>九寨沟</td>
+                        <td>5</td>
+                        <td>15%</td>
+                    </tr>
+                    </tbody>
+                </table>
             </div>
         </div>;
     }
