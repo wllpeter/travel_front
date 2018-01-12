c07962b56a38472bdcd91b7acdf6860595644b8b
liangliang <weiliangliang@andlinks.com>
2018-01-12 14:23:57 +0800

    搜索人群来源地

--- travel_front_server/src/main/java/com/bbdservice/sichuan/service/impl/SearchPersonResourceServiceImpl.java
+++ travel_front_server/src/main/java/com/bbdservice/sichuan/service/impl/SearchPersonResourceServiceImpl.java
@@ -1,7 +1,9 @@
 package com.bbdservice.sichuan.service.impl;
 
 import com.alibaba.fastjson.JSON;
+import com.bbdservice.sichuan.dao.ProvinceCityRepository;
 import com.bbdservice.sichuan.dao.SearchPersonResourceRepository;
+import com.bbdservice.sichuan.entity.ProvinceCity;
 import com.bbdservice.sichuan.entity.SearchHotWord;
 import com.bbdservice.sichuan.entity.SearchPersonResource;
 import com.bbdservice.sichuan.service.SearchPersonResourceService;
@@ -11,10 +13,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
+import java.math.BigDecimal;
+import java.util.*;
 
 @Service
 public class SearchPersonResourceServiceImpl implements SearchPersonResourceService{
@@ -27,12 +27,37 @@ public class SearchPersonResourceServiceImpl implements SearchPersonResourceServ
     private final String PERSON_RESOURCE = "38";
     @Autowired
     private SearchPersonResourceRepository searchPersonResourceRepository;
+    @Autowired
+    private ProvinceCityRepository provinceCityRepository;
     @Override
     public List<SearchPersonResource> getMonthData(Integer year, Integer month) {
         List<SearchPersonResource> ret = this.searchPersonResourceRepository.getMonthData(year,month);
         if(null != ret && ret.size() > 0){
             return ret;
         }
+
+        List<ProvinceCity> provinces = this.provinceCityRepository.getProvinceName();
+        List<ProvinceCity> citys = this.provinceCityRepository.getCityName();
+        Map<String,String> cityMap = new HashMap<>();
+        Map<String,BigDecimal> provinceMap = new HashMap<>();
+        String cityName = null;
+        String provinceName = null;
+        Integer parentId = null;
+        Integer provinceId = null;
+        //得到城市与省对应关系的map
+        for (ProvinceCity city: citys) {
+            cityName = city.getName();
+            parentId = (int)city.getParentId();
+            for (ProvinceCity province:provinces) {
+                provinceId = (int)province.getId();
+                if(parentId.equals(provinceId)){
+                    provinceName = province.getName();
+                    cityMap.put(cityName,provinceName);
+                }
+                continue;
+            }
+        }
+
         String secretKey = null;
         String ts = System.currentTimeMillis()+"";
         try {
@@ -71,7 +96,34 @@ public class SearchPersonResourceServiceImpl implements SearchPersonResourceServ
             srp.setMonth(month);
             ret.add(srp);
         }
-        this.searchPersonResourceRepository.save(ret);
-        return ret;
+
+        String c_name = null;
+        String p_name = null;
+        BigDecimal ratio = null;
+        //得到省占比的map
+        for(SearchPersonResource searchPersonResource:ret ){
+            c_name = searchPersonResource.getResourcePlace();
+            if(cityMap.containsKey(c_name)){
+                p_name = cityMap.get(c_name);
+                ratio = new BigDecimal(searchPersonResource.getRatio());
+                if(provinceMap.containsKey(p_name)){
+                    provinceMap.put(p_name,provinceMap.get(p_name).add(ratio));
+                }else{
+                    provinceMap.put(p_name,ratio);
+                }
+            }
+            continue;
+        }
+        List<SearchPersonResource> final_ret = new ArrayList<>();
+        for(String pro_name :provinceMap.keySet()){
+            SearchPersonResource srp = new SearchPersonResource();
+            srp.setResourcePlace(pro_name);
+            srp.setRatio(provinceMap.get(pro_name).toString());
+            srp.setYear(year);
+            srp.setMonth(month);
+            final_ret.add(srp);
+        }
+        this.searchPersonResourceRepository.save(final_ret);
+        return final_ret;
     }
 }
