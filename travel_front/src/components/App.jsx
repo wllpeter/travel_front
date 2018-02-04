a8e73ec20984db8f2c0268f513e8fabf15f34439
xingyixiang <452282988@qq.com>
2018-02-04 19:26:20 +0800

    padEnd兼容ie

--- travel_front/src/components/App.jsx
+++ travel_front/src/components/App.jsx
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import Frame from '../layouts/Frame';
 import 'mtui/style.scss';
+import {padEndToCompatible} from '../utils/tools';
+
+// padEnd 方法兼容性
+padEndToCompatible();
 
 export default class App extends Component {
     constructor(props) {
